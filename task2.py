efbd3742c549088eb8f269b5810bca1ac4c5c0e4
rakeshjr123 <68472580+rakeshjr123@users.noreply.github.com>
2025-09-02 00:49:54 +0530

    Created using Colab

---
+++ task2.py
@@ -0,0 +1,43 @@
+{
+  "nbformat": 4,
+  "nbformat_minor": 0,
+  "metadata": {
+    "colab": {
+      "provenance": [],
+      "authorship_tag": "ABX9TyM6+yNA+pHjSTzV59P4C+jw",
+      "include_colab_link": true
+    },
+    "kernelspec": {
+      "name": "python3",
+      "display_name": "Python 3"
+    },
+    "language_info": {
+      "name": "python"
+    }
+  },
+  "cells": [
+    {
+      "cell_type": "markdown",
+      "metadata": {
+        "id": "view-in-github",
+        "colab_type": "text"
+      },
+      "source": [
+        "<a href=\"https://colab.research.google.com/github/rakeshjr123/python-tasks/blob/main/task2.py\" target=\"_parent\"><img src=\"https://colab.research.google.com/assets/colab-badge.svg\" alt=\"Open In Colab\"/></a>"
+      ]
+    },
+    {
+      "cell_type": "code",
+      "execution_count": null,
+      "metadata": {
+        "id": "jQFFeb-ouD0U"
+      },
+      "outputs": [],
+      "source": [
+        "inp3 = input(\"Enter your first name : \")\n",
+        "inp4 =input(\"Enter your last name\")\n",
+        "print(f\"Hello {inp3} {inp4} Welcome to the python program\")"
+      ]
+    }
+  ]
+}
